8dd301b11a02a1e1d4886124322c6fa8dbfee58b
James Hall <iijameseh@gmail.com>
2023-05-25 18:47:29 +0000

    fix db.py

--- SanicPlus/utils/db.py
+++ SanicPlus/utils/db.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from google.cloud import datastore
+from google.cloud import firestore
 
 from utils.security import Security
 
@@ -8,103 +8,96 @@ from utils.security import Security
 class DB:
     def __init__(self, app):
         self.app = app
-        self.db = datastore.Client()
+        self.db = firestore.Client()
         self.security = Security()
         self.create_user_kind()
         self.create_post_kind()
 
     def create_user_kind(self):
-        # Create the kind "users"
-        self.db.kind("users")
+        # Create the collection "users"
+        self.db.collection("users")
 
     def create_post_kind(self):
-        # Create the kind "posts"
-        self.db.kind("posts")
+        # Create the collection "posts"
+        self.db.collection("posts")
 
     def add_user(self, username, password):
         # Generate a unique ID
         user_id = uuid4()
 
-        # Create a new user entity
-        user_entity = datastore.Entity(kind="users")
-        user_entity.update({
+        # Create a new user document
+        user_document = {
             "id": user_id,
             "username": username,
             "password": password,
-        })
-        self.db.put(user_entity)
+        }
+        self.db.collection("users").document(user_id).set(user_document)
 
     def login(self, request, username, password):
-        # Get the user entity
-        user_entity = self.db.query(kind="users").filter(
-                "username", username).get()
+        # Get the user document
+        user_document = self.db.collection("users").document(username).get()
 
         # Check if the user exists and the password is correct
-        if user_entity is not None and self.security.verify_password(
-                password, user_entity.get("password")):
-            return user_entity
+        if user_document is not None and self.security.verify_password(
+                password, user_document.get("password")):
+            return user_document
         return None
 
     def get_user_by_username(self, username):
-        # Get the user entity
-        return self.db.query(kind="users").filter("username", username).get()
+        # Get the user document
+        return self.db.collection("users").document(username).get()
 
     def get_user_by_id(self, user_id):
-        # Get the user entity
-        return self.db.query(kind="users").filter("id", user_id).get()
+        # Get the user document
+        return self.db.collection("users").document(user_id).get()
 
     def get_password(self, username):
-        # Get the user entity
-        user_entity = self.db.query(kind="users").filter(
-                "username", username).get()
+        # Get the user document
+        user_document = self.db.collection("users").document(username).get()
 
         # Check if the user exists
-        if user_entity is not None:
-            return user_entity.get("password")
+        if user_document is not None:
+            return user_document.get("password")
         return None
 
     def get_posts(self):
         # Get all posts
-        return self.db.query(kind="posts").fetch()
+        return self.db.collection("posts").get()
 
     def get_post(self, id):
-        # Get the post entity
-        return self.db.query(kind="posts").filter("id", id).get()
+        # Get the post document
+        return self.db.collection("posts").document(id).get()
 
     def create_post(self, title, description, image, user, type):
-        # Create a new post entity
-        post_entity = datastore.Entity(kind="posts")
-        post_entity.update({
+        # Create a new post document
+        post_document = {
             "title": title,
             "description": description,
             "image": image,
             "user": user,
             "type": type,
-            "created_at": datastore.DateTime.now(),
+            "created_at": firestore.Timestamp.now(),
             "updated_at": "",
-        })
-        self.db.put(post_entity)
+        }
+        self.db.collection("posts").document().set(post_document)
 
     def update_post(self, id, title, description, image, user, type,
                     created_at):
-        # Get the post entity
-        post_entity = self.db.query(kind="posts").filter("id", id).get()
+        # Get the post document
+        post_document = self.db.collection("posts").document(id).get()
 
-        # Update the post entity
-        post_entity.update({
+        # Update the post document
+        post_document.update({
             "title": title,
             "description": description,
             "image": image,
             "user": user,
             "type": type,
             "created_at": created_at,
-            "updated_at": datastore.DateTime.now(),
+            "updated_at": firestore.Timestamp.now(),
         })
-        self.db.put(post_entity)
+        self.db.collection("posts").document(id).set(post_document)
 
     def delete_post(self, id):
-        # Get the post entity
-        post_entity = self.db.query(kind="posts").filter("id", id).get()
-
-        # Delete the post entity
-        self.db.delete(post_entity)
+        # Delete the post document
+        self.db.collection("posts").document(id).delete()
