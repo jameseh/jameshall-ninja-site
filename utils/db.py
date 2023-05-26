c439b68994ad67d15f2162ee1026e5c1b56241d2
James Hall <iijameseh@gmail.com>
2023-05-26 02:40:59 +0000

    db

--- utils/db.py
+++ utils/db.py
@@ -10,16 +10,14 @@ class DB:
         self.app = app
         self.db = datastore.Client()
         self.security = Security()
-        self.create_user_kind()
-        self.create_post_kind()
 
     def create_user_kind(self):
         # Create the collection "users"
-        self.db.collection("users")
+        self.db.kind("users").create()
 
     def create_post_kind(self):
         # Create the collection "posts"
-        self.db.collection("posts")
+        self.db.kind("posts").create()
 
     def add_user(self, username, password):
         # Generate a unique ID
@@ -31,11 +29,12 @@ class DB:
             "username": username,
             "password": password,
         }
-        self.db.collection("users").document(user_id).set(user_document)
+        self.db.put(user_document)
 
     def login(self, request, username, password):
         # Get the user document
-        user_document = self.db.collection("users").document(username).get()
+        user_document = self.db.query(kind="users").filter(
+                "username", username).get()
 
         # Check if the user exists and the password is correct
         if user_document is not None and self.security.verify_password(
@@ -45,15 +44,16 @@ class DB:
 
     def get_user_by_username(self, username):
         # Get the user document
-        return self.db.collection("users").document(username).get()
+        return self.db.query(kind="users").filter("username", username).get()
 
     def get_user_by_id(self, user_id):
         # Get the user document
-        return self.db.collection("users").document(user_id).get()
+        return self.db.query(kind="users").filter("id", user_id).get()
 
     def get_password(self, username):
         # Get the user document
-        user_document = self.db.collection("users").document(username).get()
+        user_document = self.db.query(kind="users").filter(
+                "username", username).get()
 
         # Check if the user exists
         if user_document is not None:
@@ -62,11 +62,12 @@ class DB:
 
     def get_posts(self):
         # Get all posts
-        return self.db.collection("posts").get()
+        query = self.db.query(kind="posts")
+        return query.fetch()
 
     def get_post(self, id):
         # Get the post document
-        return self.db.collection("posts").document(id).get()
+        return self.db.query(kind="posts").filter("id", id).get()
 
     def create_post(self, title, description, image, user, type):
         # Create a new post document
@@ -79,12 +80,12 @@ class DB:
             "created_at": datastore.Timestamp.now(),
             "updated_at": "",
         }
-        self.db.collection("posts").document().set(post_document)
+        self.db.put(post_document)
 
     def update_post(self, id, title, description, image, user, type,
                     created_at):
         # Get the post document
-        post_document = self.db.collection("posts").document(id).get()
+        post_document = self.db.query(kind="posts").filter("id", id).get()
 
         # Update the post document
         post_document.update({
@@ -96,8 +97,8 @@ class DB:
             "created_at": created_at,
             "updated_at": datastore.Timestamp.now(),
         })
-        self.db.collection("posts").document(id).set(post_document)
+        self.db.put(post_document)
 
     def delete_post(self, id):
         # Delete the post document
-        self.db.collection("posts").document(id).delete()
+        self.db.query(kind="posts").filter("id", id).delete()
