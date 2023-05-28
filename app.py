a02b5023b90499a61c3f23f58cc0ec7d8cf2eda0
James Hall <iijameseh@gmail.com>
2023-05-28 17:16:16 +0000

    port

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from utils.db import DB
 
 # Setup app
 app = Sanic('jameshall_ninja_site')
-port = os.environ.get("PORT")
+port = 8080
 
 # Initiate database, to do: add configuration options
 db = DB(app)
