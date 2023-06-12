1e613b353b4e3ec1b71821839c78931767bdf56e
James Hall <ii.jameseh@gmail.com>
2023-06-12 12:05:24 -0400

    working on firebase auth

--- public/js/login.js
+++ public/js/login.js
@@ -1,64 +1,66 @@
 const auth = firebase.auth.getAuth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
-// Sign in with Google using the redirect method
-auth.signInWithRedirect(auth, provider);
+async function signInWithGoogle() {
+  // Sign in with Google using the redirect method
+  await auth.signInWithRedirect(auth, provider);
 
-// Check the response status code
-const statusCode = response.status;
-if (statusCode === 200) {
-  // Get the redirect result
-  const result = await auth.getRedirectResult(auth);
+  // Check the response status code
+  const statusCode = response.status;
+  if (statusCode === 200) {
+    // Get the redirect result
+    const result = await auth.getRedirectResult(auth);
 
-  // This gives you a Google Access Token. You can use it to access Google APIs.
-  const credential = auth.GoogleAuthProvider.credentialFromResult(result);
-  const token = credential.accessToken;
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = auth.GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
 
-  // The signed-in user info.
-  const user = auth.currentUser;
-  // IdP data available using getAdditionalUserInfo(result)
-  // ...
+    // The signed-in user info.
+    const user = auth.currentUser;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
 
-  // Get the refresh token.
-  const refreshToken = await auth.signInWithCredential(credential).refreshToken;
+    // Get the refresh token.
+    const refreshToken = await auth.signInWithCredential(credential).refreshToken;
 
-  // Encrypt the refresh token before storing it in a cookie.
-  const encryptedRefreshToken = encrypt(refreshToken);
+    // Encrypt the refresh token before storing it in a cookie.
+    const encryptedRefreshToken = encrypt(refreshToken);
 
-  // Set the refresh token expiration time.
-  auth.setRefreshTokenExpirationTime(refreshToken, 1000 * 60 * 60 * 24); // 1 day
+    // Set the refresh token expiration time.
+    auth.setRefreshTokenExpirationTime(refreshToken, 1000 * 60 * 60 * 24); // 1 day
 
-  // Get the user email, username, and profile pic.
-  const email = user.email;
-  const name = user.displayName;
-  const profilePicUrl = user.photoURL;
+    // Get the user email, username, and profile pic.
+    const email = user.email;
+    const name = user.displayName;
+    const profilePicUrl = user.photoURL;
    
-  // Create a cookie with the user's email, name, profile pic, and encrypted refresh token.
-  const cookie = `email=${email}; user_id=${name}; profilePicUrl=${profilePicUrl}; refreshToken=${encryptedRefreshtoken}`;
+    // Create a cookie with the user's email, name, profile pic, and encrypted refresh token.
+    const cookie = `email=${email}; user_id=${name}; profilePicUrl=${profilePicUrl}; refreshToken=${encryptedRefreshtoken}`;
 
-  // Set the cookie expiration time.
-  document.cookie = cookie;
+    // Set the cookie expiration time.
+    document.cookie = cookie;
 
-  // Check if there is a refresh token in the cookie.
-  const refreshToken = document.cookie.match(/refreshToken=(.*?);/)[1];
+    // Check if there is a refresh token in the cookie.
+    const refreshToken = document.cookie.match(/refreshToken=(.*?);/)[1];
 
-  // If there is a refresh token, decrypt it.
-  if (refreshToken) {
-    const decryptedRefreshToken = decrypt(refreshToken);
+    // If there is a refresh token, decrypt it.
+    if (refreshToken) {
+      const decryptedRefreshToken = decrypt(refreshToken);
 
-    // Check the validity of the refresh token.
-    if (auth.isRefreshTokenValid(decryptedRefreshToken)) {
-      // Sign in the user using the refresh token.
-      await auth.signInWithRefreshToken(decryptedRefreshToken);
+      // Check the validity of the refresh token.
+      if (auth.isRefreshTokenValid(decryptedRefreshToken)) {
+        // Sign in the user using the refresh token.
+        await auth.signInWithRefreshToken(decryptedRefreshToken);
 
-      // Redirect the user to the home page.
-      window.location.href = "/";
+        // Redirect the user to the home page.
+        window.location.href = "/";
+      }
     }
+  } else {
+    // Handle Errors here.
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
   }
-} else {
-  // Handle Errors here.
-  const errorMessage = await response.text();
-  throw new Error(errorMessage);
 }
 
 window.addEventListener('popstate', (event) => {
